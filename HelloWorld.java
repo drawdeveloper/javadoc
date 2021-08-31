41ed21d51244ecb1d822a9f5cb9703f271b6b1df
drawdeveloper <martins@drawbridgeautomation.com>
2021-08-31 18:18:43 +0100

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+   This is Java code.
+   File of this code should be saved with the name HelloWorld.java.
+*/
+
+public class HelloWorld {
+   //Execution of the program will be started by calling main().
+
+   public static void main(String[] args) {
+      //Print Hello World to the console.
+      System.out.println("Hello World");
+   }
+}
